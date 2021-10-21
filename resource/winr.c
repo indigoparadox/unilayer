23672eb7e7145b7a2af3f993fb0af84ead12e3e2
indigoparadox <indigo.repo@interfinitydynamics.info>
2021-10-21 19:10:09 -0400

    Dead code cleanup.

--- resource/winr.c
+++ resource/winr.c
@@ -11,26 +11,6 @@ RESOURCE_BITMAP_HANDLE resource_get_bitmap_handle( uint32_t id ) {
    return LoadBitmap( g_instance, MAKEINTRESOURCE( id ) );
 }
 
-#if 0
-#ifdef USE_JSON_MAPS
-RESOURCE_JSON_HANDLE resource_get_json_handle( uint32_t id ) {
-   char* buffer = NULL;
-   int read = 0;
-   HRSRC rsrc = NULL;
-   MEMORY_HANDLE handle_out = NULL;
-
-   debug_printf( 2, "loading json resource %u", id );
-   handle_out = memory_alloc( JSON_BUFFER_SZ + 1, 1 );
-   buffer = memory_lock( handle_out );
-   read = LoadString(
-      g_instance, MAKEINTRESOURCE( id ), &buffer, 0 );
-   debug_printf( 2, "string is %d bytes long (out of %d)",
-      read, JSON_BUFFER_SZ );
-   buffer = memory_unlock( handle_out );
-}
-#endif /* USE_JSON_MAPS */
-#endif
-
 MEMORY_PTR resource_lock_handle( MEMORY_HANDLE handle ) {
    return memory_lock( handle );
 }

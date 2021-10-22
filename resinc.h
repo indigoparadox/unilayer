74dd98b9744b4ee80fab9ad4c0d345c659e104ce
indigoparadox <indigo.repo@interfinitydynamics.info>
2021-10-21 21:12:40 -0400

    Fixed missing quote in error.

--- resinc.h
+++ resinc.h
@@ -3,11 +3,11 @@
 #define RESINC_H
 
 #if defined PLATFORM_WIN16 && defined RESOURCE_FILE
-#error "WIN16 has insufficent memory to use JSON parser.
+#error "WIN16 has insufficent memory to use JSON parser."
 #endif
 
 #if defined PLATFORM_DOS && defined RESOURCE_FILE
-#error "DOS has insufficent memory to use JSON parser.
+#error "DOS has insufficent memory to use JSON parser."
 #endif
 
 #if defined RESOURCE_DRC

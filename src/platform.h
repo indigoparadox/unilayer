1ef323bf704d52d0abb9fc18092cbea92fbc3331
indigoparadox <indigo.repo@interfinitydynamics.info>
2023-01-01 23:54:16 -0500

    Minor cleanup.

--- src/platform.h
+++ src/platform.h
@@ -169,8 +169,6 @@ loop_globals();
 #  define platform_fflush HostFFlush
 #  define platform_fclose HostFClose
 #  define after_unilayer_main() UInt32 PilotMain( UInt16 cmd, MemPtr cmdPBP, UInt16 launchFlags ) { if( sysAppLaunchCmdNormalLaunch == cmd ) { return unilayer_main( 0, NULL ); } }
-#  define platform_init( icon )
-#  define platform_shutdown()
 #  include <mtypes.h>
 loop_globals();
 #  include "memory/palmm.h"
